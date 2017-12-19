9edad162f4e7697097d61964ccb96c6cc16caad9
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2017-12-19 20:56:56 +0100

    GUI: fix some weird includes

--- src/gui/Src/Gui/DataCopyDialog.cpp
+++ src/gui/Src/Gui/DataCopyDialog.cpp
@@ -3,6 +3,7 @@
 #include "Bridge.h"
 #include <QCryptographicHash>
 #include <QTextCodec>
+#include "Configuration.h"
 
 #define AF_INET6        23              // Internetwork Version 6
 typedef PCTSTR(__stdcall* INETNTOPW)(INT Family, PVOID pAddr, wchar_t* pStringBuf, size_t StringBufSize);
